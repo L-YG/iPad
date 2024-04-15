5ff8cd58bbeacdb269916951faae10ab5e7a611e
L-YG <bpr_ent_2023@naver.com>
2024-04-16 01:04:42 +0900

    프로젝트 시작

---
+++ JS/main.js
@@ -0,0 +1,203 @@
+// FILE CONNECT
+import ipads from '../data/ipads.js';
+import navigations from '../data/navigations.js';
+
+
+// -------- BASKET-STARTER --------
+const basketStarterEl = document.querySelector('header .basket-starter');
+const basketEl = basketStarterEl.querySelector('.basket'); // 중복을 줄임
+
+// 추상화 개념
+function showBasket () {
+  basketEl.classList.add('show')
+}
+function hideBasket () {
+  basketEl.classList.remove('show')
+}
+
+basketStarterEl.addEventListener('click', function (event) {
+  // basketStarterEl을 클릭하면 밑에 정의된 window 객체도 실행되기 때문에 사용함
+  event.stopPropagation() //상위 객체, 즉 window 객체로 전파(실행)되는 것을 막음
+  if (basketEl.classList.contains('show')){
+    hideBasket()
+  } else {
+    showBasket()
+  }
+});
+basketEl.addEventListener('click', function (event) { 
+  // basket을 클릭하는 것은 곧 상속 관계에서 부모격인 basketStarter를 클릭하는 것과 같기 때문에 사용함
+  event.stopPropagation() //상위 객체, 즉 basketStarterEl 객체로 전파(실행)되는 것을 막음
+});
+window.addEventListener('click', function () {
+  hideBasket()
+});
+
+
+
+// -------- SEARCH-WRAP (애니메이션 처리) --------
+const headerEl = document.querySelector('header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; // 전개 연산자로 해체하고 배열 데이터로 변환
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input')
+const searchAutoCompletesEl = searchWrapEl.querySelector('.autoCompletes')
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]; // 전개 연산자로 해체하고 배열 데이터로 변환
+
+// 추상화 개념
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed') // HTML에 클래스 추가
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's' // 인덱스 크기만큼 시간을 더함, li 태그에 transition 태그가 있어야 delay 적용 가능함
+  })
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  setTimeout(function(){
+    searchInputEl.focus() // input에 포커스
+  },800)
+};
+
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed') // HTML에 클래스 추가
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's' // 인덱스 크기만큼 시간을 더함, li 태그에 transition 태그가 있어야 delay 적용 가능함
+  })
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  })
+  searchDelayEls.reverse() // 다시 뒤집어 줘야 다시 클릭할 때 정상적으로 위에서 부터 나타남
+  searchInputEl.value = '' // searchWrap이 열리면 입력 데이터 초기화
+};
+
+// searchStarterEl.addEventListener('click', function () {
+//   showSearch()
+// });
+// searchCloserEl.addEventListener('click', function () {
+//   hideSearch()
+// });
+// searchShadowEl.addEventListener('click', function () {
+//   hideSearch()
+// });
+// 위 코드 간소화
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+
+// shadow와 text, search-icon, search-closer은 상속 관계가 아니기 때문에
+// event.stopPropagation()를 써서 전파를 막을 필요가 없음
+searchShadowEl.addEventListener('click', hideSearch);
+
+
+
+// -------- INFO (요소의 가시성 관찰) --------
+const io = new IntersectionObserver(function (entries){ // 복수 형태의 배열 데이터가 들어옴
+  entries.forEach(function (entry) { // 관찰하는 개별적인 대상
+    if (!entry.isIntersecting){
+      return // true의 경우 false가 되고 return이 되지 않고, false인 경우 true가 되고 return이 실행됨
+    }
+    entry.target.classList.add('show')
+  });
+});
+
+// const io = new IntersectionObserver(function (entries){ // 복수 형태의 배열 데이터가 들어옴
+//   entries.forEach(function (entry) { // 관찰하는 개별적인 대상
+//     if (!entry.isIntersecting){ // true인 경우에만 실행
+//       entry.target.classList.remove('show')
+//       return // true의 경우 false가 되고 return이 되지 않고, false인 경우 true가 되고 return이 실행됨
+//     }
+//     entry.target.classList.add('show')
+//   });
+// });
+
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
+
+
+
+// -------- VIDEO (비디오 재생!) --------
+const videoEl = document.querySelector('.stage video');
+const playBtn = document.querySelector('.stage .controller--play');
+const pauseBtn = document.querySelector('.stage .controller--pause');
+
+playBtn.addEventListener('click', function () {
+  videoEl.play() // 재생 메서드
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+});
+pauseBtn.addEventListener('click', function () {
+  videoEl.pause() // 일시정지 메서드
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+});
+
+
+
+// COMPARE ('당신에게 맞는 iPad는?)
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach(function(ipad){
+  // 각 배열 데이터에 div 요소, class명 item을 추가
+  const itemEl = document.createElement('div');
+  itemEl.classList.add('item');
+
+  // 배열을 추출하고 대입 연산자를 이용하여 <li>에 하나씩 추가
+  let colorList = ''
+  ipad.colors.forEach(function(color){
+    colorList += `<li style="background-color: ${color};"></li>`
+  });
+
+  // 각 배열 데이터에서 각각의 정보를 HTML로 작성, 코드 하이라이팅 사용
+  itemEl.innerHTML = 
+    /* HTML */ `<div Class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}">"
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">￦${ipad.price.toLocaleString('en-us')}</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>`;
+
+  // 내용을 HTML로 밀어넣음
+  itemsEl.append(itemEl);
+});
+
+
+
+// NAVIGATIONS
+const navigationEl = document.querySelector('footer .navigations')
+navigations.forEach(function(nav){
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(function(map){
+    mapList += 
+    /* HTML */ `<li>
+        <a href="${map.url}">${map.name}</a>
+      </li>`
+  })
+
+  mapEl.innerHTML = 
+  /* HTML */ `<h3>
+    <span class="text">${nav.title}</span>"
+  </h3>
+  <ul>
+    ${mapList}
+  <ul>
+  `
+  //요소.append(데이터)
+  navigationEl.append(mapEl)
+})
+
+
+
+// LEGAL ▶ THIS-YEAR
+const thisYearEl = document.querySelector('footer span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
