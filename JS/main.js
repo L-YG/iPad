2c56656fe6972d9aaed64dca31d95fa9e6aec295
L-YG <bpr_ent_2023@naver.com>
2024-04-17 21:38:18 +0900

    @media 수정

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,10 @@
-// FILE CONNECT
+/* FILE CONNECT */
 import ipads from '../data/ipads.js';
 import navigations from '../data/navigations.js';
 
 
 
-// BASKET-STARTER
+/* BASKET-STARTER */
 const basketStarterEl = document.querySelector('header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket'); // 중복을 줄임
 
@@ -35,7 +35,7 @@ window.addEventListener('click', function () {
 
 
 
-// SEARCH-WRAP (애니메이션 처리 및 스크롤 고정)
+/* SEARCH-WRAP (애니메이션 처리 및 스크롤 고정) */
 const headerEl = document.querySelector('header');
 const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; // 전개 연산자로 해체하고 배열 데이터로 변환
 const searchWrapEl = headerEl.querySelector('.search-wrap');
@@ -86,11 +86,16 @@ function hideSearch() {
 // });
 // 위 코드 간소화
 searchStarterEl.addEventListener('click', showSearch);
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener('click', function (event) {
+  hideSearch()
+  event.stopPropagation()
+});
 
 // shadow와 text, search-icon, search-closer은 상속 관계가 아니기 때문에
 // event.stopPropagation()를 써서 전파를 막을 필요가 없음
 searchShadowEl.addEventListener('click', hideSearch);
+
+
 function playScroll(){
   document.documentElement.classList.remove('fixed') // HTML에 클래스 추가
 }
@@ -99,11 +104,13 @@ function stopScroll(){
 }
 
 
-// HEADER ▶ MENU-STARTER (@MEDIA 메뉴 토글 기능)
+
+/* HEADER ▶ MENU-STARTER (@MEDIA 메뉴 토글 기능) */
 const menuStarterEl = document.querySelector('header .menu-starter')
 menuStarterEl.addEventListener('click', function(){
   if(headerEl.classList.contains('menuing')){
   headerEl.classList.remove('menuing')
+  searchInputEl.value = ''
   playScroll()
   } else { 
     headerEl.classList.add('menuing')
@@ -113,7 +120,60 @@ menuStarterEl.addEventListener('click', function(){
 
 
 
-// INFO (각 글자 및 아이콘이 뷰포트에 보이면 나타내는 기능)
+/* HEADER ▶ SERRCH-INPUT (@MEDIA 헤더 검색 클릭 시 취소 나타냄) */
+const searchTextFieldEl = document.querySelector('header .search-wrap .textfield')
+const searchCancelerEl = document.querySelector('header .search-wrap .search-canceler')
+
+searchTextFieldEl.addEventListener('click', function(){
+  headerEl.classList.add('searching-mobile')
+  searchInputEl.focus()
+})
+searchCancelerEl.addEventListener('click', function(){
+  headerEl.classList.remove('searching-mobile')
+})
+
+
+
+/* 뷰포트 크기가 변경되면 class 제거 */
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740){
+    headerEl.classList.remove('searching')
+  } else {
+    headerEl.classList.remove('searching-mobile')
+    headerEl.classList.remove('menuing')
+  }
+})
+
+
+
+/* HEADER ▶ NAV (@MEDIA 메뉴 토글 버튼) */
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click', function () {
+  if (navEl.classList.contains('menuing')){
+    hideNavMenu()
+  } else {
+    showNavMenu()
+  }
+})
+navEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+
+function showNavMenu() {
+  navEl.classList.add('menuing')
+}
+function hideNavMenu() {
+  navEl.classList.remove('menuing')
+}
+
+
+
+/* INFO (각 글자 및 아이콘이 뷰포트에 보이면 나타내는 기능) */
 const io = new IntersectionObserver(function (entries){ // 복수 형태의 배열 데이터가 들어옴
   entries.forEach(function (entry) { // 관찰하는 개별적인 대상
     if (!entry.isIntersecting){
@@ -141,7 +201,7 @@ infoEls.forEach(function (el) {
 
 
 
-// -------- VIDEO (비디오 재생 기능)
+/* VIDEO (비디오 재생 기능) */
 const videoEl = document.querySelector('.stage video');
 const playBtn = document.querySelector('.stage .controller--play');
 const pauseBtn = document.querySelector('.stage .controller--pause');
@@ -159,7 +219,7 @@ pauseBtn.addEventListener('click', function () {
 
 
 
-// COMPARE ('당신에게 맞는 iPad는?)
+/* COMPARE ('당신에게 맞는 iPad는?) */
 const itemsEl = document.querySelector('section.compare .items');
 ipads.forEach(function(ipad){
   // 각 배열 데이터에 div 요소, class명 item을 추가
@@ -192,7 +252,7 @@ ipads.forEach(function(ipad){
 
 
 
-// NAVIGATIONS (네비게이션 메뉴를 불러오는 기능)
+/* NAVIGATIONS (네비게이션 메뉴를 불러오는 기능) */
 const navigationEl = document.querySelector('footer .navigations')
 navigations.forEach(function(nav){
   const mapEl = document.createElement('div')
@@ -220,6 +280,6 @@ navigations.forEach(function(nav){
 
 
 
-// LEGAL ▶ THIS-YEAR
+/* LEGAL ▶ THIS-YEAR */
 const thisYearEl = document.querySelector('footer span.this-year')
 thisYearEl.textContent = new Date().getFullYear()
